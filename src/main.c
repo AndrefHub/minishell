bf1a94b7e96dac95631b685f2a194f21cbb0d4db
Kristina Dancy <kdancy@va-d3.kzn.21-school.ru>
2022-05-03 19:33:14 +0300

    damn bro that was mad funny 😐

--- src/main.c
+++ src/main.c
@@ -1,42 +1,42 @@
 #include "../minishell.h"
 
-// void	very_important_function(int argc, char **argv, char **envp)
+// int main()
 // {
-// 	(void)argc;
-// 	(void)argv;
-// 	(void)envp;
+// 	t_list	*cocks = parse_quotes("first\"\'second\'\"end"), *lst = cocks;
+// 	while (lst)
+// 	{
+// 		printf("%s\n", (char *)(lst->content));
+// 		lst = lst->next;
+// 	}
+// 	ft_lstclear(&cocks, free);
 // }
 
-int main()
+void	very_important_function(int argc, char **argv, char **envp)
 {
-	t_list	*cocks = parse_quotes("first\"\'second\'\"end"), *lst = cocks;
-	while (lst)
-	{
-		printf("%s\n", (char *)(lst->content));
-		lst = lst->next;
-	}
-	ft_lstclear(&cocks, free);
+	(void)argc;
+	(void)argv;
+	(void)envp;
 }
 
-// int main(int argc, char **argv, char **envp)
-// {
-// 	char	*input;
-// 	char	**args;
-// 	int		code;
+int main(int argc, char **argv, char **envp)
+{
+	char	*input;
+	char	**args;
+	int		code;
 
-// 	g_msh.envp = envp;
-// 	very_important_function(argc, argv, envp);
-// 	while (1)
-// 	{
-// 		input = readline("ඞabobusඞ> ");
-// 		add_history(input);
-// 		args = parser_old(input, envp);
-// 		code = executor(args);
-// 		if (code)
-// 			printf("Error code %d\n", code);
-// 		// parser(input);
-// 		free(input);
-// 		ft_freesplit(args);
-// 	}
-// 	return 0;
-// }
+	g_msh.envp = envp;
+	very_important_function(argc, argv, envp);
+	while (1)
+	{
+		input = readline("ඞabobusඞ> ");
+		add_history(input);
+		args = parser_old(input, envp);
+		code = executor(args);
+		if (code)
+			printf("Error code %d\n", code);
+		// parser(input);
+		free(input);
+		ft_freesplit(args);
+	}
+	return 0;
+}
