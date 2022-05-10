138426e9713e0069cb256c0887f77c9f6fbd7f70
Kristina Dancy <kdancy@student.21-school.ru>
2022-05-10 10:34:36 +0300

    added pipeline (no testing whatsoever)

---
+++ src/executing/pipeline.c
@@ -0,0 +1,40 @@
+#include "../../minishell.h"
+
+void	execute(char **command, char **envp)
+{
+	char	*binary;
+	int		code;
+
+	commamd[0] = find_binary(command[0], envp);
+	code = execve(command[0], command, envp);
+	ft_freesplit(args);
+	if (code < 0)
+		ft_exit_message(binary, 2);
+}
+
+void    pipeline(t_command *to_pipe)
+{
+	t_command   *tmp;
+	pid_t		pid;
+	int			fd[2];
+
+	tmp = to_pipe;
+	while (tmp->link_type == PIPELINE)
+	{
+		pipe(fd);
+		pid = fork();
+		if (pid) 
+		{
+			close(fd[1]);
+			dup2(fd[0], 0);
+			if (tmp->next->link_type != PIPELINE)
+				waitpid(pid, NULL, 0);
+		}
+		else
+		{
+			close(fd[0]);
+			dup2(fd[1], 1);
+			execute(tmp->name_args, g_msh.envp);
+		}
+	}
+}
