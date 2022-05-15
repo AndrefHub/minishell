4ab12feab11b431d07080d091169b3ea405dcac3
Kristina Dancy <kdancy@student.21-school.ru>
2022-05-15 13:21:19 +0300

    "lol"

--- src/built_in/echo.c
+++ src/built_in/echo.c
@@ -6,7 +6,7 @@
 /*   By: kdancy <kdancy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 17:36:53 by kdancy            #+#    #+#             */
-/*   Updated: 2022/05/15 11:52:04 by kdancy           ###   ########.fr       */
+/*   Updated: 2022/05/15 12:50:08 by kdancy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,27 +33,37 @@ int	echo(char **argv)
 	return (1);
 }
 
+int	exit_too_many_args(void )
+{
+	ft_putstr_fd("exit: too many arguments\n", 2);
+	return (1);
+}
+
 int	msh_exit(char **argv)
 {
 	int	counter;
 
 	ft_putendl_fd("exit", 2);
-	if (argv && argv[1])
+	if (ft_arraylen((void **)argv) < 3)
 	{
-		counter = -1;
-		while (argv[1][++counter])
+		if (argv && argv[1])
 		{
-			if (!ft_isdigit(argv[1][counter]))
+			counter = -1;
+			if (argv[1][0] == '-' || argv[1][0] == '+')
+				++counter;
+			while (argv[1][++counter])
 			{
-				ft_putstr_fd("msh: ", 2);
-				ft_putstr_fd("exit: ", 2);
-				ft_putstr_fd(argv[1], 2);
-				ft_putendl_fd(": numeric argument required", 2);
-				exit(255);
+				if (!ft_isdigit(argv[1][counter]))
+				{
+					ft_putstr_fd("exit: ", 2);
+					ft_putstr_fd(argv[1], 2);
+					ft_putendl_fd(": numeric argument required", 2);
+					exit(255);
+				}
 			}
+			exit(ft_atoi(argv[1]));
 		}
-		exit(ft_atoi(argv[1]));
+		exit(g_msh.last_ex_code);
 	}
-	exit(g_msh.last_ex_code);
-	return (1);
+	return (exit_too_many_args());
 }
